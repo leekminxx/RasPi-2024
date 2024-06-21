b145fb9e39f6eb56a6392a21992414b4e940d42c
leekminxx <lkmm0712@gmail.com>
2024-06-21 17:05:00 +0900

    코드수정

--- day02/pir02.py
+++ day02/pir02.py
@@ -9,10 +9,13 @@ pirPin = 24
 ledPin = 21
 
 GPIO.setmode(GPIO.BCM)
+# GPIO.setup(pirPin, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+# 해당 핀의 풀다운 저항을 활성화
+# 풀다운 저항을 활성화하면 핀이 부동 상태일 때(센서가 신호를 보내지 않을 때) 핀의 상태가 기본적으로 LOW(0)가 됨
 GPIO.setup(pirPin, GPIO.IN)
 GPIO.setup(ledPin , GPIO.OUT)
 
-GPIO.output(ledPin, GPIO.HIGH)
+GPIO.output(ledPin, GPIO.HIGH)  
 
 try:
 	while True:
