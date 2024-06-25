645cd3c5382ef11b51ee837742673e9389d99a7f
leekminxx <lkmm0712@gmail.com>
2024-06-25 17:30:22 +0900

    세그먼트 1자리 까지 완료

--- day04/cam_flask01.py
+++ day04/cam_flask01.py
@@ -1,4 +1,4 @@
-from picamera2 import Picamera2
+from picamera2 import Picamera2, Preview
 import time
 
 picam2 = Picamera2()
@@ -6,4 +6,4 @@ camera_config = picam2.create_preview_configuration()
 picam2.configure(camera_config)
 picam2.start()
 time.sleep(2)
-picam2.capture_file("test5.jpg")
+picam2.capture_file("test1.jpg")
